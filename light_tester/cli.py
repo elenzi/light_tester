89f9cc7a4417e4e62e07efd1e4731e85383b2422
Elena Lanigan <elenalanigan93@gmail.com>
2018-03-02 00:20:22 +0000

    Commit

--- light_tester/cli.py
+++ light_tester/cli.py
@@ -6,11 +6,10 @@ import click
 
 
 @click.command()
-def main(args=None):
-    """Console script for light_tester."""
-    click.echo("Replace this message by putting your code into "
-               "light_tester.cli.main")
-    click.echo("See click documentation at http://click.pocoo.org/")
+@click.option("--input", default=None, help="input URI (file or URL)")
+def main(input=None):
+    """Console script for led_tester."""
+    print("input", input)
     return 0
 
 
--- light_tester/light_tester.py
+++ light_tester/light_tester.py
@@ -1,3 +1,24 @@
-# -*- coding: utf-8 -*-
+"""Console script for led_tester."""
+import sys
+import click
+click.disable_unicode_literals_warning = True
 
-"""Main module."""
+@click.command()
+@click.option("--input", default=None, help="input URI (file or URL)")
+def main(input=None):
+    """Console script for led_tester."""
+    print("input", input)
+
+    N, instructions = parseFile(input)
+
+    ledTester = LEDTester(N)
+
+    for instruction in instructions:
+        ledTester.apply(instruction)
+
+    print('#occupied: ', ledTester.countOccupied()) 
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())  # pragma: no cover
