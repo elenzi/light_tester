c4cac3d23b82f659b94b22fac6573f89ed55144b
Elena Lanigan <elenalanigan93@gmail.com>
2018-03-07 18:05:18 +0000

    created test for http requests

--- tests/test_light_tester.py
+++ tests/test_light_tester.py
@@ -15,3 +15,10 @@ def test_read_file():
     N, instructions = utils.parseFile(ifile)
     assert N == 10
     assert instructions == ['turn on 0,0 through 9,9\n', 'turn off 0,0 through 9,9\n', 'switch 0,0 through 9,9\n', 'turn off 0,0 through 9,9\n', 'turn on 2,2 through 7,7']
+    
+   
+def test_read_http():
+    ifile = "http://claritytrec.ucd.ie/~alawlor/comp30670/input_assign3.txt"
+    N, instructions = utils.parseFile(ifile)
+    assert N == 1000
+    assert instructions is not None
