b63a4fd5b06bbec53b68e801c6ed16899443b925
Elena Lanigan <elenalanigan93@gmail.com>
2018-03-07 15:04:23 +0000

    Tests pass to check instrctions

--- tests/test_light_tester.py
+++ tests/test_light_tester.py
@@ -1,25 +1,17 @@
- #!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-"""Tests for `light_tester` package."""
 
 
-import unittest
-import sys
-sys.path.append('.')
-import pytest
-from click.testing import CliRunner
-from light_tester import light_tester
-from light_tester import cli
+
 from light_tester import utils
+
 
 def test_command_line_interface():
     ifile = "/Users/elenalanigan/softeng/data/test.txt"
     N, instructions = utils.parseFile(ifile)
     assert N is not None
     
+    
 def test_read_file():
-    ifile = "/Users/elenalanigan/softeng/data/test.txt" 
+    ifile = "/Users/elenalanigan/softeng/data/test.txt"
     N, instructions = utils.parseFile(ifile)
     assert N == 10
-    assert instructions == ['turn on 0,0 through 9,9\n', 'turn off 0,0 through 9,9\n', 'switch 0,0 through 9,9\n', 'turn off 0,0 through 9,9\n', 'turn on 2,2 through 7,7\n']
+    assert instructions == ['turn on 0,0 through 9,9\n', 'turn off 0,0 through 9,9\n', 'switch 0,0 through 9,9\n', 'turn off 0,0 through 9,9\n', 'turn on 2,2 through 7,7']
